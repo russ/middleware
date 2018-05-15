f0c87e0822ddb66778f278ecc1abe721582e2d72
Russ Smith <russ@bashme.org>
2018-05-15 09:27:02 -0700

    Formatting.

--- spec/builder_spec.cr
+++ spec/builder_spec.cr
@@ -3,7 +3,7 @@ require "./spec_helper"
 describe Middleware::Builder do
   context "basic `use`" do
     it "should add items to the stack and make them callable" do
-      env = { "result" => [] of String }
+      env = {"result" => [] of String}
 
       instance = Middleware::Builder.new(PushHandler.new("foobar"))
       instance.call(env)
@@ -12,11 +12,11 @@ describe Middleware::Builder do
     end
 
     it "should be able to add multiple items" do
-      env = { "result" => [] of String }
+      env = {"result" => [] of String}
 
       instance = Middleware::Builder.new([
         PushHandler.new("foobar"),
-        PushHandler.new("barfoo")
+        PushHandler.new("barfoo"),
       ])
       instance.call(env)
 
@@ -26,11 +26,11 @@ describe Middleware::Builder do
   end
 
   it "should call classes in the proper order" do
-    env = { "result" => [] of String }
+    env = {"result" => [] of String}
 
     instance = Middleware::Builder.new([
       AppendPrependA.new,
-      AppendPrependB.new
+      AppendPrependB.new,
     ])
     instance.call(env)
 
